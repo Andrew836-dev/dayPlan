f57b16e9aecee8ed3237a9b6f28a636053dde506
Andrew Gray <andrew.aj.gray@gmail.com>
2020-06-16 22:32:55 +0930

    Switched my html and stylesheet to the course supplied ones, finished the script

--- script/script.js
+++ script/script.js
@@ -1,66 +1,38 @@
 $("document").ready(function () {
-    var dateEl = $("#date");
     var formatDay = 'dddd Do MMMM YYYY';
     var calendarText = [];
 
-    function updateTime() {
-        //sets the date at the top and fills the hour list
-        dateEl.text(`${moment().format(formatDay)}`);
-    }
-
     function addHour(timeIn) {
-        var rowEl = $("<div>");
-        var timeEl = $("<div>");
-        var taskEl = $("<textarea>");
-        var buttonEl = $("<button>");
-        rowEl.addClass("flex-row");
-        timeEl.addClass("timeBox");
-        timeEl.append(moment(timeIn, "HH").format("h A"));
-        taskEl.addClass("taskBox");
+        var rowEl = $("<div>").addClass("row time-block");
+        var timeEl = $("<div>").addClass("hour col-sm-1").text(moment(timeIn, "HH").format("h A"));
+        var taskEl = $("<textarea>").addClass("col-sm-10").data("index", timeIn - 9);
+        var buttonEl = $("<button>").addClass("saveBtn col-sm-1");
+        buttonEl.html('<i class="fas fa-save"></i>');
         // set class for past/present/future blocks
         if (moment().format("HH") == timeIn) {
-            taskEl.addClass("present");
+            rowEl.addClass("present");
         }
         else if (moment().format("HH") > timeIn) {
-            taskEl.addClass("past");
+            rowEl.addClass("past");
         }
         else {
-            taskEl.addClass("future");
+            rowEl.addClass("future");
         }
-        taskEl.attr("data-index", (timeIn - 9))
-        if (calendarText) {
-            taskEl.val(calendarText[timeIn - 9]);
-        }
-        else {
-            taskEl.val("");
-        }
-        buttonEl.text("Clear");
-        $("section").append(rowEl);
-        rowEl.append(timeEl);
-        rowEl.append(taskEl);
-        rowEl.append(buttonEl);
-
+        taskEl.val(calendarText[timeIn - 9]);
+        $(".container").append(rowEl);
+        rowEl.append(timeEl, taskEl, buttonEl);
     }
+
+    function updateTime() {
+        //sets the date at the top and fills the hour list
+        $("#currentDay").text(moment().format(formatDay));
+    }
+
     function clearCalendar() {
-        $("section").empty();
+        $(".container").empty();
     }
 
-    function saveCalendar() {
-        $("textarea").each(function (index) {
-            calendarText[index] = $(this).val();
-        });
-        localStorage.setItem("dayPlanAndrew", JSON.stringify(calendarText));
-    }
-
-    function loadCalendar() {
-        calendarText = JSON.parse(localStorage.getItem("dayPlanAndrew"));
-        if (!calendarText) {
-            calendarText = ["", "", "", "", "", "", "", "", "", ""]
-        }
-        fillCalendar();
-    }
-
-    function fillCalendar() {
+    function drawCalendar() {
         clearCalendar();
         updateTime();
         for (var i = 9; i < 18; i++) {
@@ -68,13 +40,23 @@ $("document").ready(function () {
         }
     }
 
+    function saveCalendar() {
+        localStorage.setItem("dayPlanAndrew", JSON.stringify(calendarText));
+    }
+
+    function loadCalendar() {
+        calendarText = JSON.parse(localStorage.getItem("dayPlanAndrew"));
+        if (!calendarText) {
+            calendarText = ["", "", "", "", "", "", "", "", "", ""]
+            saveCalendar();
+        }
+        drawCalendar();
+    }
+
     loadCalendar();
-    $("textarea").on("keyup", function () {
-        calendarText[$(this).attr("data-index")] = $(this).val().trim();
-        saveCalendar();
-    });
     $("button").on("click", function () {
-        $(this).prev().val("");
+        var textElement = $(this).prev();
+        calendarText[textElement.data("index")] = textElement.val();
         saveCalendar();
     });
 });
