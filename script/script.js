481861babe3d6acfb4d6d266f23c0c287ff1b7d8
Andrew Gray <andrew.aj.gray@gmail.com>
2020-06-16 13:21:26 +0930

    Added localstorage functions

--- script/script.js
+++ script/script.js
@@ -1,42 +1,40 @@
 $("document").ready(function () {
-    var dayEl = $("#day");
-    var formatHour = "HH";
+    var dateEl = $("#date");
     var formatDay = 'dddd Do MMMM YYYY';
+    var calendarText = [];
 
     function updateTime() {
         //sets the date at the top and fills the hour list
-        dayEl.text(`${moment().format(formatDay)}`);
+        dateEl.text(`${moment().format(formatDay)}`);
     }
 
     function addHour(timeIn) {
         var rowEl = $("<div>");
         var timeEl = $("<div>");
-        var taskEl = $("<div>");
+        var taskEl = $("<textarea>");
         var buttonEl = $("<button>");
-        timeEl.addClass("timeBox");
-        taskEl.addClass("taskBox");
         rowEl.addClass("flex-row");
+        timeEl.addClass("timeBox");
+        timeEl.append(moment(timeIn, "HH").format("h A"));
+        taskEl.addClass("taskBox");
         // set class for past/present/future blocks
-        if (moment().format(formatHour) == timeIn) {
+        if (moment().format("HH") == timeIn) {
             taskEl.addClass("present");
         }
-        else if (moment().format(formatHour) > timeIn) {
+        else if (moment().format("HH") > timeIn) {
             taskEl.addClass("past");
         }
         else {
             taskEl.addClass("future");
         }
         taskEl.attr("data-index", (timeIn - 9))
-        if (timeIn < 12) {
-            timeEl.append(timeIn + "AM");
+        if (calendarText) {
+            taskEl.val(calendarText[timeIn - 9]);
         }
         else {
-            if (timeIn > 12){
-                timeIn -= 12;
-            }
-            timeEl.append(timeIn + "PM");
+            taskEl.val("");
         }
-        buttonEl.text("Save");
+        buttonEl.text("Clear");
         $("section").append(rowEl);
         rowEl.append(timeEl);
         rowEl.append(taskEl);
@@ -47,12 +45,36 @@ $("document").ready(function () {
         $("section").empty();
     }
 
+    function saveCalendar() {
+        $("textarea").each(function (index) {
+            calendarText[index] = $(this).val();
+        });
+        localStorage.setItem("dayPlanAndrew", JSON.stringify(calendarText));
+    }
+
+    function loadCalendar() {
+        calendarText = JSON.parse(localStorage.getItem("dayPlanAndrew"));
+        if (!calendarText) {
+            calendarText = ["", "", "", "", "", "", "", "", "", ""]
+        }
+        fillCalendar();
+    }
+
     function fillCalendar() {
+        clearCalendar();
+        updateTime();
         for (var i = 9; i < 18; i++) {
             addHour(i);
         }
     }
 
-    fillCalendar();
-    updateTime();
+    loadCalendar();
+    $("textarea").on("keyup", function () {
+        calendarText[$(this).attr("data-index")] = $(this).val().trim();
+        saveCalendar();
+    });
+    $("button").on("click", function () {
+        $(this).prev().val("");
+        saveCalendar();
+    });
 });
